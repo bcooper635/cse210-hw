1f353a88aee834473e30251f55b9b3a978504ed3
bcooper635 <bdcooper@byui.edu>
2024-10-17 13:41:28 -0600

    05 Inheritance activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Assignment AS1 = new("Blake Cooper","Division");
+        Console.WriteLine(AS1.GetSummary());
+
+        MathAssignment AS2 = new("Blake Cooper","Feedback Control","4.3","46-49");
+        Console.WriteLine(AS2.GetHomeworkList());
+
+        WritingAssignment AS3 = new("Blake Cooper","Literature","Homelessness in Australia by Blake Cooper");
+        Console.WriteLine(AS3.GetWritingInformation());
     }
 }
